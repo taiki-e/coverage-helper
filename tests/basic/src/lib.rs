9f8ebe3875589b6417d7bcc98364199335c175a3
Taiki Endo <te316e89@gmail.com>
2024-05-29 03:26:44 +0900

    Ignore more lints at workspace level

--- tests/basic/src/lib.rs
+++ tests/basic/src/lib.rs
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-#![allow(unexpected_cfgs)] // for cfg(coverage_nightly)
 #![cfg_attr(all(coverage_nightly, test), feature(coverage_attribute))]
 #![allow(dead_code)]
 
